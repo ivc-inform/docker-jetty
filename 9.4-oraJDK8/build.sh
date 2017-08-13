73562f15ee7a62ac52d720fbf23fc966f429f3bd
andrew <uandrew1965@gmail.com>
2017-08-13 13:27:55 +0300

    .

--- 9.4-oraJDK8/build.sh
+++ 9.4-oraJDK8/build.sh
@@ -2,3 +2,4 @@ docker build --no-cache --force-rm --tag ivcinform/jetty:9.4.6.v20170531 .
 docker push ivcinform/jetty:9.4.6.v20170531
 
 ##docker run -d -p 8080:8080 --name jetty946_v20170531 ivcinform/jetty:9.4.6.v20170531
+docker exec -it jetty946_v20170531 bash

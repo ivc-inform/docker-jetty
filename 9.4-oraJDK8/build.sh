524ddff687c02266cfe94d3fdb384feb84e21349
andrew <uandrew1965@gmail.com>
2017-08-14 16:50:25 +0300

    minor corrects

--- 9.4-oraJDK8/build.sh
+++ 9.4-oraJDK8/build.sh
@@ -4,6 +4,6 @@ docker build --no-cache --force-rm --tag ivcinform/jetty:9.4.6.v20170531 .
 
 ##docker push ivcinform/jetty:9.4.6.v20170531
 
-docker run -it -d -p 8083:8080 --name jetty946_v20170531 ivcinform/jetty:9.4.6.v20170531 bash
+docker run -it -d -p 8083:8080 --name jetty946_v20170531 ivcinform/jetty:9.4.6.v20170531
 docker cp /home/uandrew/Job/archive-kd/web-ui/target/webapp jetty946_v20170531:/var/lib/jetty/webapps/archive-kd
 docker exec -it jetty946_v20170531 bash
